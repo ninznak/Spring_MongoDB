93d3600293b396bfda7ced728f7705796f277dd9
Kurilenko Aleksandr <me@nobito.ru>
2025-07-13 17:16:17 +0300

    added public getters and setters

--- src/main/java/alex/spring_mongodb/entityClass/User.java
+++ src/main/java/alex/spring_mongodb/entityClass/User.java
@@ -11,11 +11,44 @@ public class User {
     private String email;
     private int age;
 
-    public User() {}
+    public User() {
+    }
 
     public User(String name, String email, int age) {
         this.name = name;
         this.email = email;
+        this.age = age;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
@@ -29,3 +62,4 @@ public class User {
                 '}';
     }
 }
+
